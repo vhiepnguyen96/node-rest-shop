3f46dabfd236c8d9f322725bf8e07ca180ff0a55
Hiep Van Nguyen <vhiepnguyen96@gmail.com>
2018-11-06 15:38:43 +0700

    update checkouts

--- api/routes/checkouts.js
+++ api/routes/checkouts.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const stripe = require('stripe')(keySecret);
 
 router.post('/charge', (req, res, next) => {
-    console.log("Body: " + req.body.source);
+    console.log(req.body.email + " | " + req.body.source + " | " + req.body.amount + " | " + req.body.order);
     stripe.customers.create({
             email: req.body.email,
             source: req.body.source
@@ -34,92 +34,35 @@ router.post('/charge', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    console.log("Body: " + req.body.source);
+    console.log(req.body.email + " | " + req.body.source + " | " + req.body.amount + " | " + req.body.order);
     stripe.customers.create({
-            email: req.body.email,
+        email: req.body.email
+    }).then((customer) => {
+        console.log("CustomerID: "+customer.id);
+        return stripe.customers.createSource(customer.id, {
             source: req.body.source
-        })
-        .then(customer => {
-            stripe.charges.create({
-                    amount: req.body.amount,
-                    currency: 'usd',
-                    customer: customer.id,
-                    description: "Payment for order #" + req.body.order
-                })
-                .then(charge => {
-                    console.log("Purchase Success:", charge);
-                    return res.status(200).json(charge)
-                })
-                .catch(err => {
-                    console.log("Purchase Error:", err);
-                    return res.status(500).json(err);
-                });
-        }).catch(err => {
-            console.log("Create Customer Error:", err);
-            return res.status(500).json(err);
         });
+    }).then((source) => {
+        console.log("SourceID: "+source.id);
+        return stripe.charges.create({
+            amount: req.body.amount,
+            currency: 'usd',
+            customer: source.customer,
+            description: "Payment for order #" + req.body.order
+        });
+    }).then((charge) => {
+        console.log("ChargeID: "+charge.id);
+        res.status(200).json({
+            message: "Payment success",
+            charge: charge
+        });
+    }).catch((err) => {
+        console.log("Error: "+err);
+        res.status(500).json({
+            message: "Payment failed",
+            error: err
+        })
+    });
 });
 
 module.exports = router;
-
-// stripe.customers.retrieve({
-//     email: req.body.email
-// })
-// .then((customer) => {
-//     stripe.customers.createSource(customer.id, {
-//             source: req.body.source
-//         })
-//         .then((source) => {
-//             stripe.charges.create({
-//                     amount: req.body.amount,
-//                     currency: "usd",
-//                     source: source,
-//                     description: "Payment for order #" + req.body.order,
-//                 })
-//                 .then(charge => {
-//                     console.log("Charge detail:", charge);
-//                     res.send(charge);
-//                 })
-//                 .catch(err => {
-//                     console.log("Purchase Error:", err);
-//                     res.status(500).send({
-//                         error: "Purchase Failed"
-//                     });
-//                 });
-//         })
-//         .catch(err => {
-//             console.log("Create Source Error:", err);
-//             res.status(500).send({
-//                 error: "Create Source Failed"
-//             });
-//         });
-// }).catch((err) => {
-//     stripe.customers.create({
-//             email: req.body.email,
-//             source: req.body.source
-//         })
-//         .then(customer => {
-//             stripe.charges.create({
-//                     amount: req.body.amount,
-//                     currency: "usd",
-//                     customer: customer.id,
-//                     description: "Payment for order #" + req.body.order,
-//                 })
-//                 .then(charge => {
-//                     console.log("Charge detail:", charge);
-//                     res.send(charge);
-//                 })
-//                 .catch(err => {
-//                     console.log("Purchase Error:", err);
-//                     res.status(500).send({
-//                         error: "Purchase Failed"
-//                     });
-//                 });
-//         })
-//         .catch(err => {
-//             console.log("Create Customer Error:", err);
-//             res.status(500).send({
-//                 error: "Create Customer Failed"
-//             });
-//         });
-// });
